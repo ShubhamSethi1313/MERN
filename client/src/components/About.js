50b014b3db998b38b3a1d900d6fef30dad3dff7a
ShubhamSethi1313 <87796889+ShubhamSethi1313@users.noreply.github.com>
2023-01-23 19:07:42 +0530

    Update About.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      {/* <div className="alert alert-success d-flex pt-1 mt-0" role="alert">
+           <div className="alert alert-success d-flex pt-1 mt-0" role="alert">
         <p className="m-auto alert-about">
           <strong className="m-auto">Success!</strong> You have signed in to
           your account. Happy Coding!
@@ -15,7 +15,6 @@ const About = () => {
           aria-label="Close"
         ></button>
       </div>
-
       <div className="d-grid gap-2 col-6 mx-auto mt-5 justify-content-">
         <h1 className="about-heading">Welcome to freeCodeCamp.org</h1>
 
@@ -30,32 +29,32 @@ const About = () => {
               </footer>
             </span>
           </blockquote>
-          
         </div>
         <button className="btn btn-primary btn-about" type="button">
-        (New) Responsive Web Design Certification
+          (New) Responsive Web Design Certification
         </button>
         <button className="btn btn-primary btn-about" type="button">
-        JavaScript Algorithms and Data Structures Certification
+          JavaScript Algorithms and Data Structures Certification
         </button>
         <button className="btn btn-primary btn-about" type="button">
-        Front End Development Libraries Certification
+          Front End Development Libraries Certification
         </button>
         <button className="btn btn-primary btn-about" type="button">
-        Data Visualization Certification
+          Data Visualization Certification
         </button>
         <button className="btn btn-primary btn-about" type="button">
-        Relational Database (Beta) Certification
+          Relational Database (Beta) Certification
         </button>
         <button className="btn btn-primary btn-about" type="button">
-        Back End Development and APIs Certification
+          Back End Development and APIs Certification
         </button>
         <button className="btn btn-primary btn-about" type="button">
-        Quality Assurance Certification
+          Quality Assurance Certification
         </button>
+      </div>
 
-             </div> */}
     </div>
+    
   );
 };
 
